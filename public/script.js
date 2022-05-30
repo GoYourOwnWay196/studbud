987f3da26a5b0d132c2524e9974a87a682dfc57b
jkwo3287 <jkwo3287@uni.sydney.edu.au>
2022-05-31 08:32:07 +1000

    Update script for stopwatch interacrtivity

--- public/script.js
+++ public/script.js
@@ -158,3 +158,95 @@ function displayTimer(){
 
  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
 }
+
+// http://talkerscode.com/webtricks/create-custom-audio-player-using-html5-and-javascript.php
+
+document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
+var player;
+
+function startplayer() 
+{
+ player = document.getElementById('music_player');
+ player.controls = false;
+}
+
+function play_aud() 
+{
+ player.play();
+} 
+function pause_aud() 
+{
+ player.pause();
+}
+function stop_aud() 
+{
+ player.pause();
+ player.currentTime = 0;
+}
+function change_vol()
+{
+ player.volume=document.getElementById("change_vol").value;
+}
+
+// https://karthikdevarticles.com/creating-a-kanban-board-with-html-css-and-javascript
+function drag(ev) {
+            ev.dataTransfer.setData("text", ev.target.id);
+        }
+
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            ev.currentTarget.appendChild(document.getElementById(data));
+        }
+
+        function createTask(){
+            var x = document.getElementById("inprogress");
+            var y = document.getElementById("done");
+            var z = document.getElementById("create-new-task-block");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+                y.style.display = "block";
+                z.style.display = "none";
+            } else {
+                x.style.display = "none";
+                y.style.display = "none";
+                z.style.display = "flex";
+            }
+        }
+
+        function saveTask(){
+            // var saveButton = document.getElementById("save-button");
+            // var editButton = document.getElementById("edit-button");
+            // if (saveButton.style.display === "none") {
+            //     saveButton.style.display = "block";
+            //     editButton.style.display = "none";
+            // } else{
+            //     saveButton.style.display = "none";
+            //     editButton.style.display = "block";
+            // }
+
+            var todo = document.getElementById("todo");
+            var taskName = document.getElementById("task-name").value;
+            todo.innerHTML += `
+            <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+                <span>${taskName}</span>
+            </div>
+            `
+        }
+
+        function editTask(){
+            var saveButton = document.getElementById("save-button");
+            var editButton = document.getElementById("edit-button");
+            if (saveButton.style.display === "none") {
+                saveButton.style.display = "block";
+                editButton.style.display = "none";
+            } else{
+                saveButton.style.display = "none";
+                editButton.style.display = "block";
+            }
+        }
+        
