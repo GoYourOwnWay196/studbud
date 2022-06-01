0d87ae46c5e42308c8d4699931be64992a2225a6
jkwo3287 <jkwo3287@uni.sydney.edu.au>
2022-06-02 00:51:11 +1000

    fixing dictionary code

--- public/script.js
+++ public/script.js
@@ -1,3 +1,50 @@
+// https://codegyan.in/articles/build-a-dictionary-app-in-html-css-javascript.htm
+// Dictionary starts here
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+// Click to find word meaning
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            // Display result in html, formatted through sound, meaning and example used in a sentence
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+
+            // Error for non-existent word
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound() {
+    sound.play();
+}
+
+
 // http://talkerscode.com/webtricks/create-custom-audio-player-using-html5-and-javascript.php
 // Music player starts here
 document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
@@ -247,48 +294,3 @@ function drag(ev) {
             }
         }
 
-// https://codegyan.in/articles/build-a-dictionary-app-in-html-css-javascript.htm
-// Dictionary starts here
-
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-const btn = document.getElementById("search-btn");
-
-// Click to find word meaning
-btn.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-
-            // Display result in html, formatted through sound, meaning and example used in a sentence
-            result.innerHTML = `
-            <div class="word">
-                    <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
-                </div>
-                <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>/${data[0].phonetic}/</p>
-                </div>
-                <p class="word-meaning">
-                   ${data[0].meanings[0].definitions[0].definition}
-                </p>
-                <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
-                </p>`;
-            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
-        })
-        .catch(() => {
-
-            // Error for non-existent word
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
-        });
-});
-function playSound() {
-    sound.play();
-}
