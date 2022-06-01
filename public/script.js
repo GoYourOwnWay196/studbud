364a8a5f855b09e6d957a4798f21b565d8e79c86
jkwo3287 <jkwo3287@uni.sydney.edu.au>
2022-06-02 00:31:05 +1000

    fixed dictionary??

--- public/script.js
+++ public/script.js
@@ -1,43 +1,3 @@
-// https://codegyan.in/articles/build-a-dictionary-app-in-html-css-javascript.htm
-
-const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-const btn = document.getElementById("search-btn");
-
-btn.addEventListener("click", () => {
-    let inpWord = document.getElementById("inp-word").value;
-    fetch(`${url}${inpWord}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            result.innerHTML = `
-            <div class="word">
-                    <h3>${inpWord}</h3>
-                    <button onclick="playSound()">
-                        <i class="fas fa-volume-up"></i>
-                    </button>
-                </div>
-                <div class="details">
-                    <p>${data[0].meanings[0].partOfSpeech}</p>
-                    <p>/${data[0].phonetic}/</p>
-                </div>
-                <p class="word-meaning">
-                   ${data[0].meanings[0].definitions[0].definition}
-                </p>
-                <p class="word-example">
-                    ${data[0].meanings[0].definitions[0].example || ""}
-                </p>`;
-            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
-        })
-        .catch(() => {
-            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
-        });
-});
-function playSound() {
-    sound.play();
-}
-
 // http://talkerscode.com/webtricks/create-custom-audio-player-using-html5-and-javascript.php
 // Music player starts here
 document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
@@ -286,3 +246,43 @@ function drag(ev) {
                 editButton.style.display = "block";
             }
         }
+
+// https://codegyan.in/articles/build-a-dictionary-app-in-html-css-javascript.htm
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value;
+    fetch(`${url}${inpWord}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            result.innerHTML = `
+            <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onclick="playSound()">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                   ${data[0].meanings[0].definitions[0].definition}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+            sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+        })
+        .catch(() => {
+            result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+        });
+});
+function playSound() {
+    sound.play();
+}
