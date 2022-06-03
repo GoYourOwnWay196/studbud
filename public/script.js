5456bfd1c1072404ff3a6be5a63dba241f657efa
jkwo3287 <jkwo3287@uni.sydney.edu.au>
2022-06-03 11:26:49 +1000

    ???

--- public/script.js
+++ public/script.js
@@ -44,6 +44,68 @@ function playSound() {
     sound.play();
 }
 
+// https://karthikdevarticles.com/creating-a-kanban-board-with-html-css-and-javascript
+// Task Manager starts here
+function drag(ev) {
+            ev.dataTransfer.setData("text", ev.target.id);
+        }
+
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            ev.currentTarget.appendChild(document.getElementById(data));
+        }
+
+        function createTask(){
+            var x = document.getElementById("inprogress");
+            var y = document.getElementById("done");
+            var z = document.getElementById("create-new-task-block");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+                y.style.display = "block";
+                z.style.display = "none";
+            } else {
+                x.style.display = "none";
+                y.style.display = "none";
+                z.style.display = "flex";
+            }
+        }
+
+        function saveTask(){
+            var saveButton = document.getElementById("save-button");
+            var editButton = document.getElementById("edit-button");
+            if (saveButton.style.display === "none") {
+                 saveButton.style.display = "block";
+                 editButton.style.display = "none";
+            } else{
+                 saveButton.style.display = "none";
+                 editButton.style.display = "block";
+            }
+
+            var todo = document.getElementById("todo");
+            var taskName = document.getElementById("task-name").value;
+            todo.innerHTML += `
+            <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
+                <span>${taskName}</span>
+            </div>
+            `
+        }
+
+        function editTask(){
+            var saveButton = document.getElementById("save-button");
+            var editButton = document.getElementById("edit-button");
+            if (saveButton.style.display === "none") {
+                saveButton.style.display = "block";
+                editButton.style.display = "none";
+            } else{
+                saveButton.style.display = "none";
+                editButton.style.display = "block";
+            }
+        }
 
 // TalkersCode. (2022, April 15). Create custom audio player using HTML5 and JavaScript. TalkersCode.com - A Web Development And Internet Marketing Blog. Retrieved June 3, 2022, from http://talkerscode.com/webtricks/create-custom-audio-player-using-html5-and-javascript.php 
 // Music player starts here
@@ -231,67 +293,3 @@ function displayTimer(){
 
  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
 }
-
-// https://karthikdevarticles.com/creating-a-kanban-board-with-html-css-and-javascript
-// Task Manager starts here
-function drag(ev) {
-            ev.dataTransfer.setData("text", ev.target.id);
-        }
-
-        function allowDrop(ev) {
-            ev.preventDefault();
-        }
-
-        function drop(ev) {
-            ev.preventDefault();
-            var data = ev.dataTransfer.getData("text");
-            ev.currentTarget.appendChild(document.getElementById(data));
-        }
-
-        function createTask(){
-            var x = document.getElementById("inprogress");
-            var y = document.getElementById("done");
-            var z = document.getElementById("create-new-task-block");
-            if (x.style.display === "none") {
-                x.style.display = "block";
-                y.style.display = "block";
-                z.style.display = "none";
-            } else {
-                x.style.display = "none";
-                y.style.display = "none";
-                z.style.display = "flex";
-            }
-        }
-
-        function saveTask(){
-            var saveButton = document.getElementById("save-button");
-            var editButton = document.getElementById("edit-button");
-            if (saveButton.style.display === "none") {
-                 saveButton.style.display = "block";
-                 editButton.style.display = "none";
-            } else{
-                 saveButton.style.display = "none";
-                 editButton.style.display = "block";
-            }
-
-            var todo = document.getElementById("todo");
-            var taskName = document.getElementById("task-name").value;
-            todo.innerHTML += `
-            <div class="task" id="${taskName.toLowerCase().split(" ").join("")}" draggable="true" ondragstart="drag(event)">
-                <span>${taskName}</span>
-            </div>
-            `
-        }
-
-        function editTask(){
-            var saveButton = document.getElementById("save-button");
-            var editButton = document.getElementById("edit-button");
-            if (saveButton.style.display === "none") {
-                saveButton.style.display = "block";
-                editButton.style.display = "none";
-            } else{
-                saveButton.style.display = "none";
-                editButton.style.display = "block";
-            }
-        }
-
